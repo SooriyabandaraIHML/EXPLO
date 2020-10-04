ae8724b5445d27616f7642e97345fbe9c7a88eb8
Sathyanjali <IT19171166@my.sliit.lk>
2020-10-04 22:18:36 +0530

    travel organization

---
+++ app/src/main/java/com/example/myproject/firstpage.java
@@ -0,0 +1,35 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class firstpage extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstpage);
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(firstpage.this, StarterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+}
